10f90ddd0009b52f7bb5232dc38b5bac775c9289
fsampson30 <fsampson30@gmail.com>
2024-07-24 07:28:12 -0300

    Created component to control the layout state.

---
+++ src/main/frontend/src/layout/redux.js
@@ -0,0 +1,19 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const layoutSlice = createSlice({
+    name: 'layout',
+    initialState: {
+        drawerOpen: true,
+    },
+    reducers: {
+        toggleDrawer: state => {
+            state.drawerOpen = !state.drawerOpen;
+        }
+    }
+});
+
+export const {
+    toggleDrawer
+} = layoutSlice.actions;
+
+export const {reducer} = layoutSlice;
