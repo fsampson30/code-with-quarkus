4c7f547d15b5b4462bb8c0772c9f198221d51126
fsampson30 <fsampson30@gmail.com>
2024-07-31 07:39:39 -0300

    Created login page.

--- src/main/frontend/src/store.js
+++ src/main/frontend/src/store.js
@@ -1,14 +1,20 @@
-import {combineReducers, configureStore} from '@reduxjs/toolkit';
+import {combineReducers, configureStore, getDefaultMiddleware} from '@reduxjs/toolkit';
 import {reducer as layoutReducer} from './layout';
+import { logout } from './auth/redux';
 
 const appReducer = combineReducers({
+  auth: authReducer,
   layout: layoutReducer
 });
 
 const rootReducer = (state, action) => {
+  if (logout.match(action)) {
+    state = undefined;
+  }
   return appReducer(state, action);
 };
 
 export const store = configureStore({
-  reducer: rootReducer
+  reducer: rootReducer,
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(userApi.middleware)
 });
